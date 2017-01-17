db5b40a3b098b8ca3f10c9c8471118f913b860aa
Raphael Geissler <raphael.geissler@gmail.com>
2017-01-17 17:51:52 +0100

    Added build infrastructure.

---
+++ src/de.ragedev.eclipse.dynamictoolbar.tests/src/de/ragedev/eclipse/dynamictoolbar/SimpleTest.java
@@ -0,0 +1,12 @@
+package de.ragedev.eclipse.dynamictoolbar;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleTest {
+@Test
+	public void testName() throws Exception {
+	
+}
+}
